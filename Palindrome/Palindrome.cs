678a35fde3a3d9e715f0d251b594dd87c00a6eb9
mitali2608 <mitalibawankule21@gmail.com>
2021-10-22 22:43:15 +0530

    Program for palindrome added

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+           
+            int num2 = 0, num1 = 0;
+            int i = 0;
+            Console.WriteLine("Program to get Prime Palindrome number");
+            Console.WriteLine("----------------------------------");
+
+            for (i = 0; i <= 1000; i++)
+            {
+                num1 = i;
+                num2 = 0;
+                while (num1 != 0)
+                {
+                    int rem = num1 % 10;
+                    num1 /= 10;
+                    num2 = num2 * 10 + rem;
+                }
+
+                   if (i == num2 && i%2!=0)
+                    Console.WriteLine(i + " ");
+            }
+
+
+
+        }
+
+    }
+}
+  
+
+   
+ 
+
