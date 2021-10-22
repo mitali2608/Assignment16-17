833c36e70e3ea6a8a375d7de68209c6f5c27594a
mitali2608 <mitalibawankule21@gmail.com>
2021-10-22 22:47:33 +0530

    Program for generic search added

---
+++ GenericSearch/GenericSearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericLinarySearch
+{
+    public class GenericSearch<T> where T : IComparable
+    {
+        public T[] value;
+        public T searched_key;
+        public GenericSearch(T[] value, T searched_key)
+        {
+            this.value = value;
+            this.searched_key = searched_key;
+        }
+
+        public bool search(T[] values, T key)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+
+
+                if (values[i].Equals(key))
+                {
+                    return true;
+                }
+
+            }
+                return false;
+            
+        }
+
+       
+
+        public void PrintSearchValue()
+        {
+            
+            if (search(value,searched_key))
+            {
+                Console.WriteLine("Found value : "+searched_key);
+            }
+            else
+            {
+                Console.WriteLine(" Not Found "+searched_key);
+
+            }
+
+        }
+    }
+}
